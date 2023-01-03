ca76e4758cea3e01ed34171057dd593c5b1d50a4
NathanAlexander1 <72030740+NathanAlexander1@users.noreply.github.com>
2023-01-03 14:35:15 -0800

    update

--- controllers/animalController.js
+++ controllers/animalController.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { Animal } = require('../models/Animal.js');
+const { Animal } = require('../models');
 
 router.get("/", (req, res) => {
     Animal.findAll().then(allAnimals => {
