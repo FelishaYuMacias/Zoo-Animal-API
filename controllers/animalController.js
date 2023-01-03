8c9d19fad5bb7c476e472ac9a7981c1c9008cfbc
Felisha <felisha.j.yu@gmail.com>
2023-01-03 11:48:33 -0800

    fixed typo

--- controllers/animalController.js
+++ controllers/animalController.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { Animal } = require('../models');
+const { Animal } = require('../models/Animal.js');
 
 router.get("/", (req, res) => {
     Animal.findAll().then(allAnimals => {
